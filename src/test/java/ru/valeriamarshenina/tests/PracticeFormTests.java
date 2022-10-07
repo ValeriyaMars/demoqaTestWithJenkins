6f98a107693097e398f0758aed2a3203b9dcae9a
ValeriyaMars <valeriya.solovova@simbirsoft.com>
2022-10-07 09:48:59 +0400

    Added SystemProperties and credentials

--- src/test/java/ru/valeriamarshenina/tests/PracticeFormTests.java
+++ src/test/java/ru/valeriamarshenina/tests/PracticeFormTests.java
@@ -1,19 +1,29 @@
 package ru.valeriamarshenina.tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import ru.valeriamarshenina.tests.properties.SystemPropertiesTests;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
+import static java.lang.String.format;
 import static ru.valeriamarshenina.tests.TestData.*;
 
 public class PracticeFormTests extends tests.TestBase {
+    String url = System.getProperty("url");
+    String sourceUrl = format("https://demoqa.com/%s", url);
 
     @Test
-    void fillPracticeForm() {
-        open("https://demoqa.com/automation-practice-form");
+    @Tag("allInputs")
+    void fillPracticeFormAllInputs() {
+        System.setProperty("browser", "chrome");
+        System.setProperty("version", "91");
+        System.setProperty("browserSize", "500x500");
+        System.setProperty("url","automation-practice-form");
 
+        open(sourceUrl);
         $("#firstName").setValue(studentFirstName);
         $("#lastName").setValue(studentLastName);
         $("#userEmail").setValue(studentEmail);
@@ -43,6 +53,122 @@ public class PracticeFormTests extends tests.TestBase {
         $(".modal-body").shouldHave(text("Picture 1.png"));
         $(".modal-body").shouldHave(text("Address " + studentCurrentAddress));
         $(".modal-body").shouldHave(text("State and City NCR Delhi"));
+    }
 
+    @Test
+    @Tag("withoutNotRequiredInputs")
+    void fillPracticeFormMaleWithoutEmail() {
+        System.setProperty("browser", "chrome");
+        System.setProperty("version", "91");
+        System.setProperty("browserSize", "500x500");
+        System.setProperty("url","automation-practice-form");
+
+        open(sourceUrl);
+        $("#firstName").setValue(studentFirstName);
+        $("#lastName").setValue(studentLastName);
+        $(byText("Female")).click();
+        $("#userNumber").setValue(studentNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("8");
+        $(".react-datepicker__year-select").selectOptionByValue("1988");
+        $x("//div[contains(@aria-label, \"September 28th, 1988\")]").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $(byText("Reading")).click();
+        $("input#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").scrollTo().setValue(studentCurrentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").scrollTo().click();
+
+        $(".modal-body").shouldHave(text("Student Name " + studentFirstName + " " + studentLastName));
+        $(".modal-body").shouldHave(text("Student Email"));
+        $(".modal-body").shouldHave(text("Gender Female"));
+        $(".modal-body").shouldHave(text("Mobile " + studentNumber));
+        $(".modal-body").shouldHave(text("Date of Birth 28 September,1988"));
+        $(".modal-body").shouldHave(text("Subjects Maths"));
+        $(".modal-body").shouldHave(text("Hobbies Reading"));
+        $(".modal-body").shouldHave(text("Picture 1.png"));
+        $(".modal-body").shouldHave(text("Address " + studentCurrentAddress));
+        $(".modal-body").shouldHave(text("State and City NCR Delhi"));
+    }
+
+    @Test
+    @Tag("withoutNotRequiredInputs")
+    void fillPracticeFormMaleWithoutSubjects() {
+        System.setProperty("browser", "chrome");
+        System.setProperty("version", "91");
+        System.setProperty("browserSize", "500x500");
+        System.setProperty("url","automation-practice-form");
+
+        open(sourceUrl);
+        $("#firstName").setValue(studentFirstName);
+        $("#lastName").setValue(studentLastName);
+        $("#userEmail").setValue(studentEmail);
+        $(byText("Female")).click();
+        $("#userNumber").setValue(studentNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("8");
+        $(".react-datepicker__year-select").selectOptionByValue("1988");
+        $x("//div[contains(@aria-label, \"September 28th, 1988\")]").click();
+        $(byText("Reading")).click();
+        $("input#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").scrollTo().setValue(studentCurrentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").scrollTo().click();
+
+        $(".modal-body").shouldHave(text("Student Name " + studentFirstName + " " + studentLastName));
+        $(".modal-body").shouldHave(text("Student Email " + studentEmail));
+        $(".modal-body").shouldHave(text("Gender Female"));
+        $(".modal-body").shouldHave(text("Mobile " + studentNumber));
+        $(".modal-body").shouldHave(text("Date of Birth 28 September,1988"));
+        $(".modal-body").shouldHave(text("Subjects"));
+        $(".modal-body").shouldHave(text("Hobbies Reading"));
+        $(".modal-body").shouldHave(text("Picture 1.png"));
+        $(".modal-body").shouldHave(text("Address " + studentCurrentAddress));
+        $(".modal-body").shouldHave(text("State and City NCR Delhi"));
+    }
+
+    @Test
+    @Tag("withoutNotRequiredInputs")
+    void fillPracticeFormMaleWithoutCurrentAddress() {
+        System.setProperty("browser", "chrome");
+        System.setProperty("version", "91");
+        System.setProperty("browserSize", "500x500");
+        System.setProperty("url","automation-practice-form");
+
+        open(sourceUrl);
+        $("#firstName").setValue(studentFirstName);
+        $("#lastName").setValue(studentLastName);
+        $("#userEmail").setValue(studentEmail);
+        $(byText("Female")).click();
+        $("#userNumber").setValue(studentNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("8");
+        $(".react-datepicker__year-select").selectOptionByValue("1988");
+        $x("//div[contains(@aria-label, \"September 28th, 1988\")]").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $(byText("Reading")).click();
+        $("input#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-body").shouldHave(text("Student Name " + studentFirstName + " " + studentLastName));
+        $(".modal-body").shouldHave(text("Student Email " + studentEmail));
+        $(".modal-body").shouldHave(text("Gender Female"));
+        $(".modal-body").shouldHave(text("Mobile " + studentNumber));
+        $(".modal-body").shouldHave(text("Date of Birth 28 September,1988"));
+        $(".modal-body").shouldHave(text("Subjects Maths"));
+        $(".modal-body").shouldHave(text("Hobbies Reading"));
+        $(".modal-body").shouldHave(text("Picture 1.png"));
+        $(".modal-body").shouldHave(text("Address"));
+        $(".modal-body").shouldHave(text("State and City NCR Delhi"));
     }
 }
--- src/test/java/ru/valeriamarshenina/tests/TestBase.java
+++ src/test/java/ru/valeriamarshenina/tests/TestBase.java
@@ -3,23 +3,39 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.valeriamarshenina.config.CredentialsConfig;
 import ru.valeriamarshenina.helpers.Attach;
+
+import static java.lang.String.format;
 
 
 public class TestBase {
-    @BeforeAll
-    static void setup() {
+
+    public CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+    String login = credentials.login();
+    String password = credentials.password();
+
+    @BeforeEach
+    void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
+        System.setProperty("url", "selenoid.autotests.cloud/wd/hub/");
+        String selenoidUrl = System.getProperty("url");
+        String browserUrl = format("https://%s:%s%s", login, password, selenoidUrl);
+
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = browserUrl;
     }
 
     @AfterEach
